8fba63e9f24c6a317bb01cbe92fcd0f794fcfcfe
Lucas Soares <polonio230@hotmail.com>
2024-05-21 17:25:43 -0300

    build: config TarefaControllerIntegrationTest

--- src/test/java/com/todolist/integration/TarefaControllerIntegrationTest.java
+++ src/test/java/com/todolist/integration/TarefaControllerIntegrationTest.java
@@ -3,36 +3,45 @@ package com.todolist.integration;
 import com.todolist.ToDoListApplication;
 import io.restassured.RestAssured;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.Matchers.equalTo;
-@RunWith(JUnitPlatform.class)
+@ExtendWith(SpringExtension.class)
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest(classes = {ToDoListApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = {ToDoListApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class TarefaControllerIntegrationTest {
 
-    @Before
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
     public void setup() {
-        RestAssured.baseURI = "http://localhost:8080";
-        RestAssured.port = 8080;
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
     }
+
 
     @Test
     public void givenUrl_whenSuccessOnGetsResponseAndJsonHasRequiredKV_thenCorrect() { get("/api/tarefa").then().statusCode(200);
     }
 
-
-    @Test public void givenUrl_whenSuccessOnGetsResponseAndJsonHasOneTask_thenCorrect() { get("/api/tarefa?titulo" +
-            "=Primeira tarefa").then().statusCode(200) .assertThat().body("titulo", equalTo("Primeira tarefa")); }
-
-
+    @Test
+    public void givenUrl_whenSuccessOnGetsResponseAndJsonHasOneTask_thenCorrect() {
+        get("/api/tarefa?titulo=Primeira tarefa").then().statusCode(200)
+                .assertThat().body("tarefas[0].titulo", equalTo("Primeira tarefa"));
+    }
 
 
 
